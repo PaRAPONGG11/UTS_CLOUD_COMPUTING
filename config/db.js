2b8e2cbcb47a5a2417d970f97cea281912d9b617
PaRAPONGG11 <ipengtoxe@gmail.com>
2026-04-22 21:39:50 +0700

    "perubahan db.js"

--- config/db.js
+++ config/db.js
@@ -1,30 +1,16 @@
-// const mysql = require('mysql2/promise');
-// require('dotenv').config();
-
-// const pool = mysql.createPool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT || 3306,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//     enableKeepAlive: true,
-//     keepAliveInitialDelay: 0
-// });
-
-// module.exports = pool;
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: "sampah.caza4y6k4bni.us-east-1.rds.amazonaws.com", // Masukkan endpoint tanpa spasi/http
-    user: "admin", // Gunakan user asli kamu
-    password: "Password_RDS_Kamu", // Masukkan password aslinya
-    database: "sampah",
+    host: process.env.RDS_HOST,
+    user: process.env.RDS_USER,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
 module.exports = pool;
